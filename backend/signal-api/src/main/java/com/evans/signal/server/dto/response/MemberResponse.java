0063b5d6d34c5eaaa39e391881eddd2a642af94f
Evans <akrap147@naver.com>
2026-02-04 13:25:07 +0900

    refactor: Role Enum 도입 및 Member 도메인 리팩토링

--- backend/signal-api/src/main/java/com/evans/signal/server/dto/response/MemberResponse.java
+++ backend/signal-api/src/main/java/com/evans/signal/server/dto/response/MemberResponse.java
@@ -3,11 +3,13 @@ package com.evans.signal.server.dto.response;
 import lombok.Builder;
 import lombok.Getter;
 
+import com.evans.signal.server.domain.Role;
+
 @Getter
 @Builder
 public class MemberResponse {
     private Long id;
     private Long userId; // 실제 유저 ID
-    private String role; // OWNER, MEMBER etc.
+    private Role role; // OWNER, MEMBER etc.
     // 추후 nickname, avatarUrl 등 추가 예정
 }
